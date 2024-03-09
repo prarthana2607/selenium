53d4d28d455f53a1e5da5e91a70320dd3abd3864
Admin <Admin@DESKTOP-BAMKGB2>
2024-03-09 13:28:31 +0530

    first commit

---
+++ src/test/java/TESTNG/assert1.java
@@ -0,0 +1,13 @@
+package TESTNG;
+
+public class assert1 { 
+	@Test
+	public void Test01() {
+		SoftAssert s=new SoftAssert();
+		
+		
+		
+		
+	}                                                                                                  nnnnnnnnnnnn                                        
+
+}
